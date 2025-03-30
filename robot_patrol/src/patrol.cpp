f387aaceb7fd70c63e32cbf820f3df2955fb0d95
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-03-30 18:21:35 +0000

    Found laser rays corresponding to each direction

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,8 +1,16 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include <vector>
 
 using std::placeholders::_1;
+
+// Number of laser rays : 660
+// Laser scan ranges vector correspondances:
+// 0-360 : BACK
+// 165   : RIGHT
+// 330   : FRONT
+// 465   : LEFT
 
 class Patrol : public rclcpp::Node {
 
@@ -16,20 +24,17 @@ public:
 
 private:
   void scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    // print number of laser rays = 294
-    RCLCPP_INFO(this->get_logger(), " length of ranges: '%lx'",
-                msg->ranges.size());
-    RCLCPP_INFO(this->get_logger(), " length of intensities: '%lx'",
-                msg->intensities.size());
 
-    // RCLCPP_INFO(this->get_logger(), " angle_min : '%f'", msg->angle_min);
-    // RCLCPP_INFO(this->get_logger(), " angle_max : '%f'", msg->angle_max);
+    // RCLCPP_INFO(this->get_logger(), " length of ranges: '%lx'",
+    //             msg->ranges.size());
+    std::cout << "MAX number of rays" << msg->ranges.max_size() << std::endl;
+    std::cout << "number of rays" << msg->ranges.size() << std::endl;
 
     RCLCPP_INFO(this->get_logger(), " ranges[0]: '%f'", msg->ranges[0]);
-    RCLCPP_INFO(this->get_logger(), " ranges[73]: '%f'", msg->ranges[73]);
-    RCLCPP_INFO(this->get_logger(), " ranges[146]: '%f'", msg->ranges[146]);
-    RCLCPP_INFO(this->get_logger(), " ranges[219]: '%f'", msg->ranges[219]);
-    RCLCPP_INFO(this->get_logger(), " ranges[293]: '%f'", msg->ranges[293]);
+    RCLCPP_INFO(this->get_logger(), " ranges[165]: '%f'", msg->ranges[165]);
+    RCLCPP_INFO(this->get_logger(), " ranges[330]: '%f'", msg->ranges[330]);
+    RCLCPP_INFO(this->get_logger(), " ranges[495]: '%f'", msg->ranges[495]);
+    RCLCPP_INFO(this->get_logger(), " ranges[660]: '%f'", msg->ranges[659]);
   }
 
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
