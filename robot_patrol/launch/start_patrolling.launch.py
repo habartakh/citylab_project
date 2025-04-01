a30d7a39676944e6457786fa050b18f82728b71f
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-04-01 11:56:07 +0000

    Added rviz2 launch

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -6,15 +6,17 @@ from ament_index_python.packages import get_package_share_directory
 
 # launch rviz2 node from https://github.com/ros2/rviz/issues/627
 def generate_launch_description():
-    # base_path = os.path.realpath(get_package_share_directory('robot_patrol')) # also tried without realpath
-    # rviz_path=base_path+'/config/config.rviz'
+    base_path = os.path.realpath(get_package_share_directory('robot_patrol')) 
+    #print(base_path)
+    rviz_path=base_path+'/config/config.rviz'
+    #print(rviz_path)
     
     return LaunchDescription([
         launch_ros.actions.Node(
              package='robot_patrol',
             executable='robot_patrol_node', output='screen'),
-        # launch_ros.actions.Node(
-        #     package='rviz2',
-        #     executable='rviz2',  arguments=['-d',str(rviz_path)]
-        # ),
+        launch_ros.actions.Node(
+            package='rviz2',
+            executable='rviz2',output='screen', arguments=['-d', str(rviz_path)]
+        ),
     ])
