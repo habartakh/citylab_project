4e3eb0f15fbf5ef4021dc35b113e153d7d5af785
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-04-06 21:38:01 +0000

    Created a new node doing the patrol by calling the service /direction_service

---
+++ robot_patrol/launch/main.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+
+def generate_launch_description():
+        
+    return LaunchDescription([
+        launch_ros.actions.Node(
+             package='robot_patrol',
+            executable='direction_service_node', output='screen'),
+            launch_ros.actions.Node(
+             package='robot_patrol',
+            executable='patrol_with_service_node', output='screen'),
+        
+    ])
---
+++ robot_patrol/src/patrol_with_service.cpp
@@ -0,0 +1,155 @@
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+#include "robot_patrol/srv/get_direction.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+using GetDirection = robot_patrol::srv::GetDirection;
+
+class Patrol : public rclcpp::Node {
+
+public:
+  Patrol() : Node("robot_patrol_node") {
+
+    // Create a subscriber to /scan topic
+    scan_sub_callback_group_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    options.callback_group = scan_sub_callback_group_;
+    laser_scan_subscription_ =
+        this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "scan", 10, std::bind(&Patrol::scan_topic_callback, this, _1),
+            options);
+
+    // create a publisher to move the robot
+    move_robot_publisher =
+        this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+    timer_callback_group_ = this->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive);
+    timer_ = this->create_wall_timer(
+        100ms, std::bind(&Patrol::timer_callback, this), timer_callback_group_);
+
+    client_ = this->create_client<GetDirection>("direction_service");
+  }
+
+private:
+  void scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    laser_scan_msg = *msg;
+    this->twist_msg.angular.z = 0.0;
+    this->twist_msg.linear.x = 0.1;
+
+    if (obstacle_detected_forward()) {
+      send_async_request(); // call the service to get next direction to move to
+
+      if (direction_ == "forward") {
+        this->twist_msg.angular.z = 0.0;
+      }
+
+      if (direction_ == "right") {
+        this->twist_msg.angular.z = -0.5;
+      }
+      if (direction_ == "left") {
+        this->twist_msg.angular.z = 0.5;
+      }
+    }
+
+    move_robot_publisher->publish(this->twist_msg);
+  }
+
+  // we will verify existance of obstacles ahead using all the rays between
+  // [-30°,30°] corresponding to laser_scan_msg.ranges[275:385]
+  bool obstacle_detected_forward() {
+    bool obstacle_detected = false;
+    if (!laser_scan_msg.ranges.empty()) {
+      for (auto it = laser_scan_msg.ranges.begin() + 275;
+           it != laser_scan_msg.ranges.begin() + 386; it++) {
+        if (*it < 0.35) {
+          obstacle_detected = true;
+        }
+      }
+    }
+    return obstacle_detected;
+  }
+
+  // move robot forward till detecting an obstacle less than 35cm
+  // However, it is problematic to use only 1 ray to verify existence of
+  // obstacles, instead we use obstacle_detected_forward method
+  void timer_callback() {}
+
+  void send_async_request() {
+    while (!client_->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(
+            this->get_logger(),
+            "Client interrupted while waiting for service. Terminating...");
+        return;
+      }
+      RCLCPP_INFO(this->get_logger(),
+                  "Service Unavailable. Waiting for Service...");
+    }
+
+    auto request = std::make_shared<GetDirection::Request>();
+    request->laser_data = laser_scan_msg;
+    auto result_future = client_->async_send_request(
+        request,
+        std::bind(&Patrol::response_callback, this, std::placeholders::_1));
+
+    // Now check for the response after a timeout of 1 second
+    auto status = result_future.wait_for(1s);
+
+    if (status != std::future_status::ready) {
+
+      RCLCPP_WARN(this->get_logger(), "Response not ready yet.");
+    }
+  }
+
+  // will be called when the service returns a response
+  void response_callback(rclcpp::Client<GetDirection>::SharedFuture future) {
+    // Get response value
+    auto response = future.get();
+    direction_ = response->direction;
+    RCLCPP_INFO(this->get_logger(), "Response: %s \n",
+                response->direction.c_str());
+    service_done_ = true;
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+      laser_scan_subscription_;
+  rclcpp::CallbackGroup::SharedPtr scan_sub_callback_group_;
+  rclcpp::SubscriptionOptions options;
+
+  rclcpp::Client<GetDirection>::SharedPtr client_;
+  sensor_msgs::msg::LaserScan laser_scan_msg;
+
+  std::string direction_; // safest direction robot can take to complete patrol
+  bool service_done_ = false;
+
+  geometry_msgs::msg::Twist twist_msg;
+
+  rclcpp::CallbackGroup::SharedPtr timer_callback_group_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr move_robot_publisher;
+};
+
+int main(int argc, char **argv) {
+
+  rclcpp::init(argc, argv);
+  // rclcpp::spin(std::make_shared<Patrol>());
+  std::shared_ptr<Patrol> patrol_node = std::make_shared<Patrol>();
+
+  // This is the same as a print in ROS
+  RCLCPP_INFO(patrol_node->get_logger(), "Patrol Node STARTING ...");
+
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(patrol_node);
+  executor.spin();
+
+  rclcpp::shutdown();
+
+  return 0;
+}
